43304378f31f4689cef55e4b4a37a68107d7f380
Pedro Neto <pedroneto@Pedros-MacBook-Air.local>
2019-01-19 13:34:47 -0300

    active admin on

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+
+AdminUser.create!(email: 'pjsn@cin.ufpe.br', password: '12341234', password_confirmation: '12341234')
+AdminUser.create!(email: 'samuelmataraso@gmail.com', password: '12341234', password_confirmation: '12341234')
+AdminUser.create!(email: 'hugo@jambotech.com.br', password: '12341234', password_confirmation: '12341234')
