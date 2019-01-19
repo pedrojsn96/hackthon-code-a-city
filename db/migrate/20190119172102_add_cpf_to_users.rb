69eb1db352d9e2b1e41f097241633c81cfe478cd
Pedro Neto <pedroneto@Pedros-MacBook-Air.local>
2019-01-19 14:24:47 -0300

    adding  users info

---
+++ db/migrate/20190119172102_add_cpf_to_users.rb
@@ -0,0 +1,7 @@
+class AddCpfToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :cpf, :string
+    add_column :users, :phone, :string
+    add_column :users, :birth_on, :date
+  end
+end
