74ceac0b9f2529d847dbd1f42394de592291d428
Pedro Neto <pedroneto@Pedros-MacBook-Air.local>
2019-01-19 18:31:04 -0300

    create relation user and course

--- app/admin/course_registrations.rb
+++ app/admin/course_registrations.rb
@@ -9,22 +9,21 @@ ActiveAdmin.register CourseRegistration do
     index do
         selectable_column
         id_column
-        column :users do |c|
-          c.name
+        column :user do |c|
+            c.user.name
         end
         column :course do |c|
-            c.title
-          end
+            c.course.title
+        end
+        column :status
         column :created_at
         actions
       end
 
       form do |f|
-        f.inputs "Detalhes" do
-          f.input :user, :as => :select, collection: User.order(:id).all.map(&:id)
-          f.input :course, :as => :select, collection: Course.order(:id).all.map(&:id)
-          f.input :status
-        end
+        f.input :user, :as => :select, collection: User.all.map{|u| ["#{u.name}", u.id]}
+        f.input :course, :as => :select, collection: Course.all.map{|c| ["#{c.title}", c.id]}
+        f.input :status
         f.actions
       end
 end
